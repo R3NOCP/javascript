815ee21b08b96ce2ae24559a6d1471b99720b843
Reno Postema <postemareno@gmail.com>
2019-05-17 17:24:33 +0200

    Create html + javascript

---
+++ javascript
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Understanding Arithmetic</h2>
+<p id="arithMetic"></p>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,5 @@
+function arithMetic(sum) {
+  return "Outcome 6 + 50 * 10 =  " + sum;
+}
+
+document.getElementById("arithMetic").innerHTML = arithMetic(6 + 50 * 10);
