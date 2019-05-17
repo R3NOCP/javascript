383ad4f1eeb15eae7a04cfe4b8d1af5ca23e7d2b
Reno Postema <postemareno@gmail.com>
2019-05-17 17:29:20 +0200

    Add remainder to script

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function arithMetic(sum) {
 }
 
 document.getElementById("arithMetic").innerHTML = arithMetic(6 + 50 * 10);
+
+var a = 5;
+var b = 2;
+var c = a % b;
+document.getElementById("remainder").innerHTML = "What is left 5 % 2 =  " + c;
